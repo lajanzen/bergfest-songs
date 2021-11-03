38814b4d9f865dc99420139e35dae4ace9193e25
lajanzen <lara-janzen@hotmail.com>
2021-11-03 17:13:10 +0100

    Log input value in login form

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Form.module.css';
 
 export default function Form(): JSX.Element {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  console.log(firstName);
+  console.log(lastName);
+
   return (
     <form className={styles.form}>
       <select className={styles.dropdown}>
@@ -10,8 +16,18 @@ export default function Form(): JSX.Element {
         <option>Lara Janzen</option>
       </select>
       <span>- or create a new user -</span>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+      <input
+        type="text"
+        placeholder="Username"
+        value={firstName}
+        onChange={(event) => setFirstName(event.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={lastName}
+        onChange={(event) => setLastName(event.target.value)}
+      />
       <button className={styles.button} type="submit">
         Log-In
       </button>
