d91a00ba6a28003b149ef0325c936050ecb605c3
lajanzen <lara-janzen@hotmail.com>
2021-11-03 10:39:40 +0100

    Add welcome page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
-  return <main>Hello Fish</main>;
+  return (
+    <main className={styles.container}>
+      <img
+        className={styles.image}
+        src="https://images.unsplash.com/photo-1429962714451-bb934ecdc4ec?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+        alt=""
+      />
+      <div>
+        <h1>Bergfest</h1>
+        <form className={styles.form}>
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Log-In</button>
+        </form>
+      </div>
+    </main>
+  );
 }
 
 export default App;
