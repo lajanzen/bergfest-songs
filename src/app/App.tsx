bb2f194e0719c03d8b8c23a4053441db125f842f
lajanzen <lara-janzen@hotmail.com>
2021-11-04 15:17:56 +0100

    Control components and lift state up

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './App.module.css';
 import Form from './components/Form/Form';
 import Title from './components/Title/Title';
 
 function App(): JSX.Element {
+  const [selectedUserName, setSelectedUserName] = useState('');
+
+  console.log(selectedUserName);
+
   return (
     <main className={styles.container}>
       <div className={styles.login}>
         <Title text="Bergfest" />
-        <Form />
+        <Form value={selectedUserName} onChange={setSelectedUserName} />
       </div>
     </main>
   );
--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,9 +1,21 @@
 import React, { useState } from 'react';
 import styles from './Form.module.css';
 
-export default function Form(): JSX.Element {
+type FormProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+type User = {
+  firstName: string;
+  lastName: string;
+};
+
+export default function Form({ onChange }: FormProps): JSX.Element {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+  // const [selectedUser, setSelectedUser] = useState('');
 
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -20,12 +32,33 @@ export default function Form(): JSX.Element {
     });
   }
 
+  async function handleSelectClick() {
+    const response = await fetch('https://json-server.machens.dev/users');
+    const newUsers = await response.json();
+    setUsers(newUsers);
+  }
+
+  const userOptions = users.map((user) => (
+    <option>
+      {user.firstName} {user.lastName}
+    </option>
+  ));
+
+  // function handleChange(event: React.ChangeEvent<HTMLSelectElement>) {
+  //   setSelectedUser(event.target.value);
+  // }
+
+  // console.log(selectedUser);
+
   return (
     <form className={styles.form} onSubmit={handleSubmit}>
-      <select className={styles.dropdown}>
+      <select
+        className={styles.dropdown}
+        onClick={handleSelectClick}
+        onChange={(event) => onChange(event.target.value)}
+      >
         <option disabled>Select a fish</option>
-        <option>Leon Machens</option>
-        <option>Lara Janzen</option>
+        {userOptions}
       </select>
       <span>- or create a new user -</span>
       <input
